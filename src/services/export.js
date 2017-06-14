fe94c9e0db781cba4d17b960f2dfa11e9338c6f1
Roman <zummo3@gmail.com>
2017-06-14 23:22:47 +0300

    Fix export due to changes in ComputerNetwork structure

--- src/services/export.js
+++ src/services/export.js
@@ -2,7 +2,7 @@ import { saveAs } from 'file-saver'
 
 export default function exportStructure (stack) {
   const stackItems = Array.from(stack)
-  const result = stackItems.map((s) => ({ id: s.id, network: Array.from(s.network) }))
+  const result = stackItems.map((s) => ({ id: s.id, computers: Array.from(s.computers) }))
   const jsonContent = JSON.stringify(result, null, 2)
 
   const blob = new Blob([jsonContent], { type: 'text/plain;charset=utf-8' })
