149b29c8b644f4a7578039b71ee3fca67991dc5b
Roman <zummo3@gmail.com>
2017-06-14 22:14:35 +0300

    Refactor and improve test coverage for LinkedList

--- src/structures/LinkedList.js
+++ src/structures/LinkedList.js
@@ -8,60 +8,72 @@ class LinkedListItem {
 
 export default class LinkedList {
   constructor () {
-    this._head = null
+    this._firstItem = null
+    this._lastItem = null
     this.length = 0
   }
 
   isEmpty () {
-    return this._head === null
+    return this.length === 0
   }
 
   addByIndex (index, data) {
-    this.length++
-    if (this.isEmpty()) {
-      this._addFirstElement(data)
+    let newItem
+    if (index === 0) {
+      newItem = new LinkedListItem(null, this._firstItem, data)
+    } else if (index === this.length) {
+      newItem = new LinkedListItem(this._lastItem, null, data)
     } else {
-      const itemToModify = this._find(index)
-      const previousItem = itemToModify.previous
-
-      const newItem = new LinkedListItem(previousItem, itemToModify, data)
-
-      previousItem.next = newItem
-      itemToModify.previous = newItem
-
-      if (index === 0) {
-        this._head = newItem
-      }
+      const itemToInsertAfter = this._find(index).previous
+      newItem = new LinkedListItem(itemToInsertAfter, itemToInsertAfter.next, data)
     }
+
+    if (newItem.next) {
+      newItem.next.previous = newItem
+    } else {
+      this._lastItem = newItem
+    }
+
+    if (newItem.previous) {
+      newItem.previous.next = newItem
+    } else {
+      this._firstItem = newItem
+    }
+
+    this.length++
   }
 
   cleanUp () {
-    let i = this._head.next
-    while (i !== this._head) {
+    let i = this._firstItem
+    while (i) {
       i.previous = null
       const nextItem = i.next
       i.next = null
       i = nextItem
     }
-    this._head = null
-    this.length = 0
+    this._firstItem = null
+    this._lastItem = null
   }
 
   removeByIndex (index) {
-    this.length--
-    if (index === 0) {
-      const itemToRemove = this._head
+    const itemToRemove = this._find(index)
+    const prevItem = itemToRemove.previous
+    const nextItem = itemToRemove.next
 
-      this._head = this._head.next
-      this._head.previous = itemToRemove.previous
-      this._head.previous.next = this._head
-    } else {
-      const itemToModify = this._find(index)
-      const previousItem = itemToModify.previous
-
-      previousItem.next = itemToModify.next
-      itemToModify.next = previousItem
+    if (prevItem) {
+      prevItem.next = nextItem
     }
+    if (nextItem) {
+      nextItem.previous = prevItem
+    }
+    if (index === 0) {
+      this._firstItem = nextItem
+    }
+    if (index === this.length - 1) {
+      this._lastItem = prevItem
+    }
+
+    this.length--
   }
 
   editByIndex (index, newData) {
@@ -73,24 +85,71 @@ export default class LinkedList {
     return this._find(index).data
   }
 
+  toArrayBackward () {
+    let result = []
+    let currentItem = this._lastItem
+    if (currentItem) {
+      do {
+        result.push(currentItem.data)
+        currentItem = currentItem.previous
+      } while (currentItem !== null)
+    }
+
+    return result
+  }
+
+  searchBy (property, searchQuery) {
+    let currentItem = this._firstItem
+    let index = 0
+
+    if (currentItem) {
+      do {
+        if (currentItem.data[property] === searchQuery) {
+          return {
+            index,
+            result: currentItem.data
+          }
+        }
+        currentItem = currentItem.next
+        index++
+      } while (currentItem !== null)
+    }
+
+    throw new Error('Element not found')
+  }
+
+  searchBackwardBy (property, searchQuery) {
+    let currentItem = this._lastItem
+    let index = 0
+
+    if (currentItem) {
+      do {
+        if (currentItem.data[property] === searchQuery) {
+          return {
+            index,
+            result: currentItem.data
+          }
+        }
+        currentItem = currentItem.previous
+        index++
+      } while (currentItem !== null)
+    }
+
+    throw new Error('Element not found')
+  }
+
   *[Symbol.iterator] () {
-    let currentItem = this._head
+    let currentItem = this._firstItem
     if (currentItem) {
       do {
         yield currentItem.data
         currentItem = currentItem.next
-      } while (currentItem !== this._head)
+      } while (currentItem !== null)
     }
   }
 
-  _addFirstElement (data) {
-    this._head = new LinkedListItem(null, null, data)
-    this._head.previous = this._head
-    this._head.next = this._head
-  }
-
   _find (index) {
-    let result = this._head
+    let result = this._firstItem
     for (let i = 0; i <= index; i++) {
       if (result === null) {
         throw new Error('Invalid index')
--- src/structures/__tests__/LinkedList.js
+++ src/structures/__tests__/LinkedList.js
@@ -9,10 +9,7 @@ describe('addByIndex', () => {
     list.addByIndex(3, 4)
     list.addByIndex(2, 5)
 
-    let result = []
-    for (let i of list) {
-      result.push(i)
-    }
+    let result = Array.from(list)
 
     expect(result).toEqual([1, 3, 5, 2, 4])
   })
@@ -26,10 +23,7 @@ describe('removeByIndex', () => {
     list.addByIndex(2, 3)
     list.removeByIndex(1)
 
-    let result = []
-    for (let i of list) {
-      result.push(i)
-    }
+    let result = Array.from(list)
 
     expect(result).toEqual([1, 3])
   })
@@ -40,11 +34,145 @@ describe('removeByIndex', () => {
     list.addByIndex(1, 2)
     list.removeByIndex(0)
 
-    let result = []
-    for (let i of list) {
-      result.push(i)
-    }
+    let result = Array.from(list)
 
     expect(result).toEqual([2])
+  })
+
+  it('works [3]', () => {
+    const list = new LinkedList()
+    list.addByIndex(0, 1)
+    list.addByIndex(1, 2)
+    list.addByIndex(2, 3)
+    list.removeByIndex(2)
+    list.removeByIndex(0)
+
+    let result = Array.from(list)
+
+    expect(result).toEqual([2])
+  })
+
+  it('works [4]', () => {
+    const list = new LinkedList()
+    list.addByIndex(0, 1)
+    list.removeByIndex(0)
+    list.addByIndex(0, 2)
+    list.addByIndex(1, 3)
+
+    let result = Array.from(list)
+
+    expect(result).toEqual([2, 3])
+  })
+
+  it('works [5]', () => {
+    const list = new LinkedList()
+    list.addByIndex(0, 1)
+    list.addByIndex(1, 2)
+    list.addByIndex(2, 3)
+    list.removeByIndex(1)
+    list.addByIndex(1, 5)
+
+    const result = Array.from(list)
+
+    expect(result).toEqual([1, 5, 3])
+  })
+})
+
+describe('isEmpty', () => {
+  it('works properly for empty list', () => {
+    const list = new LinkedList()
+    expect(list.isEmpty()).toBeTruthy()
+  })
+
+  it('works properly for not empty list', () => {
+    const list = new LinkedList()
+    list.addByIndex(0, 1)
+    expect(list.isEmpty()).toBeFalsy()
+  })
+})
+
+describe('editByIndex', () => {
+  it('works', () => {
+    const list = new LinkedList()
+    list.addByIndex(0, 1)
+    list.addByIndex(1, 2)
+    list.addByIndex(2, 3)
+    list.editByIndex(1, 100)
+
+    const result = Array.from(list)
+
+    expect(result).toEqual([1, 100, 3])
+  })
+})
+
+describe('getByIndex', () => {
+  it('works', () => {
+    const list = new LinkedList()
+    list.addByIndex(0, 1)
+    list.addByIndex(1, 2)
+    list.addByIndex(2, 3)
+
+    expect(list.getByIndex(1)).toEqual(2)
+  })
+})
+
+describe('toArrayBackward', () => {
+  it('works', () => {
+    const list = new LinkedList()
+    list.addByIndex(0, 1)
+    list.addByIndex(1, 2)
+    list.addByIndex(2, 3)
+    list.removeByIndex(1)
+    list.addByIndex(1, 5)
+
+    const result = list.toArrayBackward()
+
+    expect(result).toEqual([3, 5, 1])
+  })
+})
+
+describe('searchBy', () => {
+  it('works', () => {
+    const list = new LinkedList()
+    list.addByIndex(0, { name: 'a', foo: 'bar1' })
+    list.addByIndex(1, { name: 'b', foo: 'bar2'  })
+    list.addByIndex(2, { name: 'c', foo: 'bar3'  })
+
+    expect(list.searchBy('name', 'c')).toEqual({
+      index: 2,
+      result: { name: 'c', foo: 'bar3' }
+    })
+  })
+
+  it('throws error if element is not found', () => {
+    const list = new LinkedList()
+    list.addByIndex(0, { name: 'a', foo: 'bar1' })
+    list.addByIndex(1, { name: 'b', foo: 'bar2'  })
+    list.addByIndex(2, { name: 'c', foo: 'bar3'  })
+
+    expect(() => { list.searchBy('name', 'd') }).toThrowError('Element not found')
+  })
+})
+
+describe('searchBackwardBy', () => {
+  it('works', () => {
+    const list = new LinkedList()
+    list.addByIndex(0, { name: 'a', foo: 'bar1' })
+    list.addByIndex(1, { name: 'b', foo: 'bar2'  })
+    list.addByIndex(2, { name: 'c', foo: 'bar3'  })
+
+    expect(list.searchBackwardBy('name', 'c')).toEqual({
+      index: 0,
+      result: { name: 'c', foo: 'bar3' }
+    })
+  })
+
+  it('throws error if element is not found', () => {
+    const list = new LinkedList()
+    list.addByIndex(0, { name: 'a', foo: 'bar1' })
+    list.addByIndex(1, { name: 'b', foo: 'bar2'  })
+    list.addByIndex(2, { name: 'c', foo: 'bar3'  })
+
+    expect(() => { list.searchBackwardBy('name', 'd') }).toThrowError('Element not found')
   })
 })
